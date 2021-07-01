c988e725d99bac938256c0f14f5292c508534e30
liscli39 <suppo1999@gmail.com>
2021-07-01 11:41:26 +0700

    Update MS1

--- MS1/Time.cpp
+++ MS1/Time.cpp
@@ -1,5 +1,147 @@
 #include "Time.h"
 #include "utils.h"
 using namespace std;
-namespace sdds {
+namespace sdds
+{
+  Time &Time::setToNow()
+  {
+    m_min = getTime();
+  }
+  Time::Time(unsigned int min)
+  {
+    m_min = min;
+  }
+  ostream &Time::write(ostream &ostr) const
+  {
+    int minute = m_min % 60;
+    int hour = (m_min - minute) / 60;
+
+    if (hour < 10) {
+      ostr << "0" << hour;
+    } else {
+      ostr << hour;
+    }
+    ostr << ":";
+    if (minute < 10) {
+      ostr << "0" << minute;
+    } else {
+      ostr << minute;
+    }
+
+    return ostr;
+  }
+  istream &Time::read(istream &istr)
+  {
+    char c = '\n';
+    int l = 0;
+    int hour = 0;
+    int minute = 0;
+    bool change = false;
+
+    do
+    {
+      istr.get(c);
+      if (c != '\n' && ((c >= '0' && c <= '9') || c == ':'))
+      {
+        if (c == ':')
+        {
+          change = true;
+        }
+        else if (change)
+        {
+          minute = minute * 10 + (c - '0');
+        }
+        else
+        {
+          hour = hour * 10 + (c - '0');
+        }
+      }
+      else if (c != '\n')
+      {
+        istr.setstate(ios::failbit);
+      }
+    } while (c != '\n' && ((c >= '0' && c <= '9') || c == ':'));
+
+    if (!istr.fail()) {
+      m_min = hour * 60 + minute;
+    }
+
+    return istr;
+  }
+  Time &Time::operator-=(const Time &D)
+  {
+    while (m_min < D.m_min)
+    {
+      m_min += 24 * 60;
+    }
+
+    m_min -= D.m_min;
+    return *this;
+  }
+  Time Time::operator-(const Time &D) const
+  {
+    Time t(m_min);
+
+    while (t.m_min < D.m_min)
+    {
+      t.m_min += 24 * 60;
+    }
+
+    t.m_min -= D.m_min;
+
+    return t;
+  }
+  Time &Time::operator+=(const Time &D)
+  {
+    m_min += D.m_min;
+    return *this;
+  }
+  Time Time::operator+(const Time &D) const
+  {
+    Time t(m_min + D.m_min);
+    return t;
+  }
+  Time &Time::operator=(unsigned int val)
+  {
+    m_min = val;
+    return *this;
+  }
+  Time &Time::operator*=(unsigned int val)
+  {
+    m_min *= val;
+    return *this;
+  }
+  Time &Time::operator/=(unsigned int val)
+  {
+    m_min /= val;
+    return *this;
+  }
+  Time Time::operator*(unsigned int val) const
+  {
+    Time t(m_min * val);
+    return t;
+  }
+  Time Time::operator/(unsigned int val) const
+  {
+    Time t(m_min / val);
+    return t;
+  }
+  Time::operator unsigned int() const
+  {
+    return m_min;
+  }
+  Time::operator int() const
+  {
+    return m_min;
+  }
+  ostream &operator<<(ostream &ostr, const Time &D)
+  {
+    D.write(ostr);
+    return ostr;
+  }
+  istream &operator>>(istream &istr, Time &D)
+  {
+    D.read(istr);
+    return istr;
+  }
 }
--- MS1/utils.cpp
+++ MS1/utils.cpp
@@ -7,31 +7,81 @@
 #include "utils.h"
 #include "Time.h"
 using namespace std;
-namespace sdds {
-   bool debug = false;  // made global in utils.h
-   int getTime() {  
+namespace sdds
+{
+   bool debug = false; // made global in utils.h
+   int getTime()
+   {
       int mins = -1;
-      if (debug) {
+      if (debug)
+      {
          Time t(0);
          cout << "Enter current time: ";
-         do {
+         do
+         {
             cin.clear();
-            cin >> t;   // needs extraction operator overloaded for Time
-            if (!cin) {
+            cin >> t; // needs extraction operator overloaded for Time
+            if (!cin)
+            {
                cout << "Invlid time, try agian (HH:MM): ";
                cin.clear();
+               cin.ignore(1000, '\n');
             }
-            else {
+            else
+            {
                mins = int(t);
             }
-            cin.ignore(1000, '\n');
          } while (mins < 0);
       }
-      else {
+      else
+      {
          time_t t = time(NULL);
          tm lt = *localtime(&t);
          mins = lt.tm_hour * 60 + lt.tm_min;
       }
       return mins;
+   }
+
+   int getInt(const char *prompt)
+   {
+      return 0;
+   }
+   int getInt(int min, int max, const char *prompt, const char *errorMessage, bool showRangeAtError)
+   {
+      return 0;
+   }
+
+   char *getcstr(const char *prompt, istream &istr, char delimiter)
+   {
+      char *str = nullptr;
+      char *tmp = nullptr;
+      char c;
+      int l = 0;
+      do
+      {
+         istr.get(c);
+         if (c != delimiter)
+         {
+            if (str != nullptr)
+            {
+               delete[] str;
+            }
+            str = new char[++l + 1];
+
+            if (tmp != nullptr)
+            {
+               strcpy(str, tmp);
+               delete[] tmp;
+            }
+
+            str[l - 1] = c;
+            str[l] = '\0';
+
+            tmp = new char[l + 1];
+            strcpy(tmp, str);
+         }
+      } while (c != delimiter);
+
+      return str;
    }
 }
